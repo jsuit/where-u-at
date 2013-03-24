e73a31c49c6612116fbdd77b3490c53b9be857de
Andre Giron <agiron123@gmail.com>
2013-03-24 14:13:06 -0400

    Databases Lecture

--- src/gitmad/app/WhereUAt/ListViewFragment.java
+++ src/gitmad/app/WhereUAt/ListViewFragment.java
@@ -17,21 +17,23 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class ListViewFragment extends Fragment {
 
-    //Code based off tutorial found at:
-    //http://www.vogella.com/articles/AndroidListView/article.html#listview_listviewexample
-
     ArrayList<String> values;
+    ArrayList<Place> places;
     EditText listAddTextView;
-    ArrayAdapter<String> adapter;
+    PlaceAdapter placeAdapter;
     ListView listView;
+	DatabaseHandler dbHandler;
+	int placeID;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list, container, false);
-        
         setupFragment(view);
-
+        this.dbHandler = new DatabaseHandler(this.getActivity());
+        this.placeID = this.dbHandler.getCurrentID();
+        placeID ++; //remove later, need to recreate database
+        placeID++;
         return view;
     }
 
@@ -40,17 +42,17 @@ public class ListViewFragment extends Fragment {
         listAddTextView.setText("Type some text");
         listView = (ListView) view.findViewById(R.id.locationListView);
 
+        places = new ArrayList<Place>();
+        places.add(new Place(places.size()+1,"Detroit", 1.2243, 1.234));
+        places.add(new Place(places.size()+1,"San Francisco", 1.2243, 1.234));
+
         values = new ArrayList<String>();
         values.add("Detroit");
         values.add("San Francisco");
-        values.add("Seattle");
-        values.add("New York");
-        values.add("Miami");
-        values.add("Chicago");
-            
-        //simple_list_item_1 is something that is included in android by default
-        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, values);
-        listView.setAdapter(adapter); //apply the adapter to the listview so that it can show the contents of the array
+
+        //adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, places);
+        placeAdapter = new PlaceAdapter(getActivity(), R.layout.listview_item_row, places);
+        listView.setAdapter(placeAdapter); //apply the adapter to the listview so that it can show the contents of the array
 
         //onClick Listener which displays the item that we clicked on
         listView.setOnItemClickListener(new OnItemClickListener() {
@@ -58,7 +60,7 @@ public class ListViewFragment extends Fragment {
               public void onItemClick(AdapterView<?> parent, View view,
                 int position, long id) {
                 Toast.makeText(getActivity().getApplicationContext(),
-                  "Clicked on " + values.get(position), Toast.LENGTH_LONG).show();
+                  "Clicked on " + places.get(position).getName(), Toast.LENGTH_LONG).show();
               }
             });
 
@@ -67,9 +69,18 @@ public class ListViewFragment extends Fragment {
             public void onClick(View v) {
                 // Perform action on click
                 String typed = listAddTextView.getText().toString();
-                adapter.add(typed);
-                listAddTextView.setText("");
-                adapter.notifyDataSetChanged();
+                
+                //only add a place if the user typed something
+                if(typed.length() > 0)
+                {
+	                Place entered = new Place(placeID +1,typed, 1.2243, 1.234);
+	                places.add(entered);
+	                dbHandler.addplace(entered);
+	                placeID++;
+	                listAddTextView.setText("");
+	                placeAdapter.notifyDataSetChanged();
+	                dbHandler.logPlaces();
+                }
             }
         });
     }
